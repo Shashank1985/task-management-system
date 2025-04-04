e043029e0dbc3780c7d858c47084f0be316d10b8
Shashank <shashankshashidhar73@gmail.com>
2025-04-05 01:32:36 +0530

    add middleware to verify user token

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const Uset = require('../models/User')
+
+//verify the token from the cookie 
+//this middleware will be executed before a user accesses protected routes
+//checks if the token is valid, checks if its not been tampered with by checking signature
+//redirects to login page if jwt is invalid
+exports.isAuthenticated = async (req,res,next) => { 
+    const token = req.cookies.token
+    if (!token){
+        res.redirect('/auth/login')
+    }
+    try{ 
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await User.findByID(decoded.id)
+        if(!user){
+            return res.redirect('/auth/login')
+        }
+        req.user = user
+        res.locals.user = user
+        next()
+    }catch(error){
+        console.error('Auth middleware error:', error)
+        res.clearCookie('token')
+        res.redirect('/auth/login')
+    }
+}
