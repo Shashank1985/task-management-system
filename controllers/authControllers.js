cd100bee81a2454715058d3cf62d182800787459
Shashank <shashankshashidhar73@gmail.com>
2025-04-05 12:17:56 +0530

    remove some stuff

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -41,7 +41,6 @@ exports.register = async (req, res) => {
     // Set token in cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
     
@@ -84,7 +83,6 @@ exports.login = async (req, res) => {
     // Set token in cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
     
